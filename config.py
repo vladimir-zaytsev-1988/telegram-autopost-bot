c78c1a04a85db7341a31651b6578e891d1995e30
vladimir-zaytsev-1988 <vladimir-zaytsev-1988@mail.ru>
2025-06-13 00:51:11 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,2 @@
+BOT_TOKEN = "8159576173:AAGE2XWrkmZtvzhovpdTh8yz2vHyYTREhAA"
+CHANNEL_ID = "@luchshe_kazhdy_den"
---
+++ main.py
@@ -0,0 +1,30 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from aiogram.types import InputFile
+from config import BOT_TOKEN, CHANNEL_ID
+import json
+import datetime
+
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher(bot)
+
+async def send_scheduled_posts():
+    with open("bot/posts.json", "r", encoding="utf-8") as f:
+        posts = json.load(f)
+
+    now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+    for post in posts:
+        if post["datetime"] == now:
+            photo = InputFile(f"bot/{post['image_path']}")
+            await bot.send_photo(chat_id=CHANNEL_ID, photo=photo, caption=post["text"])
+
+async def scheduler():
+    while True:
+        await send_scheduled_posts()
+        await asyncio.sleep(60)
+
+if __name__ == "__main__":
+    from aiogram import executor
+    loop = asyncio.get_event_loop()
+    loop.create_task(scheduler())
+    executor.start_polling(dp, skip_updates=True)
